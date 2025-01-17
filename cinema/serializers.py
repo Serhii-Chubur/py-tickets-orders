4826716c9b07b5260dc8bf40d7a630975793278f
Serhii-Chubur <serhichubur15@gmail.com>
2025-01-17 16:16:30 +0200

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,6 +1,13 @@
+from django.db import transaction
 from rest_framework import serializers
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
+from cinema.models import (Genre,
+                           Actor,
+                           CinemaHall,
+                           Movie,
+                           MovieSession,
+                           Ticket,
+                           Order)
 
 
 class GenreSerializer(serializers.ModelSerializer):
@@ -16,15 +23,39 @@ class ActorSerializer(serializers.ModelSerializer):
 
 
 class CinemaHallSerializer(serializers.ModelSerializer):
+    capacity = serializers.IntegerField(read_only=True)
+
     class Meta:
         model = CinemaHall
         fields = ("id", "name", "rows", "seats_in_row", "capacity")
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    genres = serializers.SlugRelatedField(
+        many=True,
+        slug_field="name",
+        read_only=True
+    )
+    actors = serializers.SlugRelatedField(
+        many=True,
+        slug_field="full_name",
+        read_only=True
+    )
+    # genres = GenreSerializer(many=True, read_only=False)
+    # actors = ActorSerializer(many=True, read_only=False)
+
     class Meta:
         model = Movie
         fields = ("id", "title", "description", "duration", "genres", "actors")
+
+    def validate(self, attrs):
+        if "actors" in attrs:
+            if not all(isinstance(actor_id, int)
+                       for actor_id in attrs["actors"]):
+                raise serializers.ValidationError(
+                    "Actors must be a list of integers (primary keys)."
+                )
+        return attrs
 
 
 class MovieListSerializer(MovieSerializer):
@@ -59,6 +90,7 @@ class MovieSessionListSerializer(MovieSessionSerializer):
     cinema_hall_capacity = serializers.IntegerField(
         source="cinema_hall.capacity", read_only=True
     )
+    tickets_available = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = MovieSession
@@ -68,13 +100,63 @@ class MovieSessionListSerializer(MovieSessionSerializer):
             "movie_title",
             "cinema_hall_name",
             "cinema_hall_capacity",
+            "tickets_available"
         )
+
+
+class TicketSerializer(serializers.Serializer):
+    row = serializers.IntegerField()
+    seat = serializers.IntegerField()
+
+    class Meta:
+        model = Ticket
+        fields = ("row", "seat")
 
 
 class MovieSessionDetailSerializer(MovieSessionSerializer):
     movie = MovieListSerializer(many=False, read_only=True)
     cinema_hall = CinemaHallSerializer(many=False, read_only=True)
+    taken_places = TicketSerializer(
+        many=True,
+        read_only=True,
+        source="tickets"
+    )
 
     class Meta:
         model = MovieSession
-        fields = ("id", "show_time", "movie", "cinema_hall")
+        fields = ("id", "show_time", "movie", "cinema_hall", "taken_places")
+
+
+class TicketListSerializer(serializers.Serializer):
+    movie_session = MovieSessionListSerializer(many=False, read_only=True)
+    row = serializers.IntegerField()
+    seat = serializers.IntegerField()
+
+    def validate(self, data):
+        ticket = Ticket(**data)
+        ticket.clean()
+
+    class Meta:
+        model = Ticket
+        fields = ("id", "row", "seat", "movie_session")
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    tickets = TicketListSerializer(
+        many=True,
+        allow_empty=False,
+        required=True,
+        read_only=False
+    )
+
+    class Meta:
+        model = Order
+        fields = "id", "created_at", "tickets"
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            tickets_data = validated_data.pop("tickets")
+            order = Order.objects.create(**validated_data)
+            for ticket_data in tickets_data:
+                Ticket.objects.create(order=order, **ticket_data)
+            return order
